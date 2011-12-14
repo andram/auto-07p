andram/auto-07p

commit 9de0bf44a9d64d773dd48f7de4502c330955fda8

Reintroduce the AUTO_COMMAND_PREFIX variable from AUTO2000 for running with MPI.
git-svn-id: https://auto-07p.svn.sourceforge.net/svnroot/auto-07p/trunk/auto/07p@2635 ee9e63dd-6847-0410-88cd-b7174c777ff7

python/runAUTO.py
@@ -368,7 +368,11 @@ class runAUTO:
                                  self.fort9_path]:
                     if os.path.exists(filename):
                         os.remove(filename)
-                self.runCommand(os.path.join(".",equation + ".exe"), constants)
+                command = os.path.join(".",equation + ".exe")
+                prefix = os.environ.get("AUTO_COMMAND_PREFIX")
+                if prefix is not None:
+                    command = " ".join((prefix, command))
+                self.runCommand(command)
                 if os.path.exists("fort.3"):
                     os.remove("fort.3")
                 line = "%s ... done\n"%equation

doc/auto.tex
@@ -54,7 +54,7 @@ Xianjun Wang,
 and
 Chenghai Zhang.\\
 }}
-\date{October 2009}
+\date{December 2011}
 \pagenumbering{alph}
 \maketitle
 \pagenumbering{arabic}
@@ -574,6 +574,23 @@ line option which defines the number of computational processes is \filef{
 on four processors, with the above external program, you would
 type \filef{mpirun -np 4 file.exe}.  Please see your local MPI
 documentation for more detail.
+
+Both the Python CLUI and the commands in the \filef{ auto/07p/cmds}
+directory described in Chapter~\ref{sec:command_mode} may be used with the
+MPI version as well, by setting the \commandf{ AUTO\_COMMAND\_PREFIX}
+environment variable.  For example, to run \AUTO in parallel using
+the MPI library on 4 processors just type
+\commandf{export AUTO\_COMMAND\_PREFIX='mpirun -np 4'}
+before you run the Python CLUI \commandf{auto} or
+the commands in \filef{ auto/07p/cmds} normally. 
+The previous example
+assumed you are using the \commandf{sh} shell or the \commandf{bash} shell; for
+other shells you should modify the commands appropriately,
+for example \commandf{setenv AUTO\_COMMAND\_PREFIX 'mpirun -np 4'}
+for the \commandf{csh} and \commandf{tcsh} shells. Alternatively,
+inside the Python
+CLUI and scripts you can use \commandf{import os} followed by
+\commandf{os.environ["AUTO\_COMMAND\_PREFIX"]="mpirun -np 4"}.
 
 %==============================================================================
 %==============================================================================

commit 1035030d7abd071a2ed2c5c413fd06ba22335d4e

Fix typo causing regression made in r2635.
git-svn-id: https://auto-07p.svn.sourceforge.net/svnroot/auto-07p/trunk/auto/07p@2636 ee9e63dd-6847-0410-88cd-b7174c777ff7

python/runAUTO.py
@@ -372,7 +372,7 @@ class runAUTO:
                 prefix = os.environ.get("AUTO_COMMAND_PREFIX")
                 if prefix is not None:
                     command = " ".join((prefix, command))
-                self.runCommand(command)
+                self.runCommand(command, constants)
                 if os.path.exists("fort.3"):
                     os.remove("fort.3")
                 line = "%s ... done\n"%equation